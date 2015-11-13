cbbaea20a61be7b98165480aef37a4824854d95f
Nikolay Paskulov <npaskul@softserveinc.com>
2015-11-13 19:02:09 +0200

    Adding sprints.

--- JiraLogWork/ViewModels/ProjectViewModel.cs
+++ JiraLogWork/ViewModels/ProjectViewModel.cs
@@ -14,6 +14,11 @@ namespace JiraLogWork.ViewModels
 		private IEnumerable<IPresentationViewModel> _presentationViewModels;
 		private Jira.REST.Jira _jira;
 		private IEnumerable<NameValueModel> _boards;
+		private IEnumerable<NameValueModel> _sprints;
+		private IEnumerable<NameValueModel> _issues;
+		private int _selectedBoard;
+		private int _selectedSprint;
+		private int _selectedIssue;
 
 		public ProjectViewModel(IProjectView view, IEnumerable<IPresentationViewModel> presentationViewModels, Jira.REST.Jira jira)
 		{
@@ -22,6 +27,24 @@ namespace JiraLogWork.ViewModels
 			this.View = view;
 			_presentationViewModels = presentationViewModels;
 			_jira = jira;
+
+			InitCommands();
+
+		}
+
+		private void InitCommands()
+		{
+			Boards_Changed = new DelegateCommand(_ =>
+			{
+				int test = SelectedBoard;
+
+				Sprints = _jira.GetBoardSprints(test.ToString())
+				.Where(x => x.State == "active")
+				.Select(x => new NameValueModel {
+					Name = x.Name,
+					Value = x.Id
+				});
+            });
 		}
 
 		public IEnumerable<NameValueModel> Boards
@@ -29,10 +52,29 @@ namespace JiraLogWork.ViewModels
 			get { return _boards; }
 			set { this.SetField(ref this._boards, value, () => this.Boards); }
 		}
+		public IEnumerable<NameValueModel> Sprints
+		{
+			get { return _sprints; }
+			set { this.SetField(ref this._sprints, value, () => this.Sprints); }
+		}
+
+		public int SelectedBoard
+		{
+			get { return _selectedBoard;  }
+			set { this.SetField(ref this._selectedBoard, value, () => this.SelectedBoard); }
+		}
+
+		private int SelectedSprint
+		{
+			get { return _selectedSprint; }
+			set { this.SetField(ref this._selectedSprint, value, () => this.SelectedSprint); }
+		}
 
 		public string Name { get; set; }
 		public Pages Page { get; set; }
 		public IView View { get; set; }
+
+		public DelegateCommand Boards_Changed { get; set; }
 
 		public void ViewChanged()
 		{
@@ -41,6 +83,6 @@ namespace JiraLogWork.ViewModels
 				Name = x.Name,
 				Value = x.Id
 			});
-		}
+        }
 	}
 }
